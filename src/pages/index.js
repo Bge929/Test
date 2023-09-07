f159bf5da609b8a8212ab9ed50031763595ffaf6
Bge <bas.gebel@btg.nl>
2023-09-07 12:43:38 +0200

    removed test in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const IndexPage = () => {
                 alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
                 src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
             />
-            <p>TEST</p>
         </Layout>
     )
 }
